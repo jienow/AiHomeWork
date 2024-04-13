49bbf6f57416e4afaeddaea5c7401325c202d81f
互联网高级技工 <2609482146@qq.com>
2024-04-14 00:00:35 +0800

    基本功能

--- main.py
+++ main.py
@@ -15,22 +15,50 @@ import cv2
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap, QImage
 from PyQt5.QtWidgets import QApplication, QDialog, QLabel, QPushButton
+from python import body_from_image
 from PyQt5.uic import loadUi
+import uuid
 
 class MyMainWindow(QDialog):
     def __init__(self):
         super().__init__()
         # 加载 .ui 文件
         loadUi('D:\\development\\python\\openpose\\ui\\untitled.ui', self)
+        self.image = None
         self.initUI()
         # 此处可以添加其他初始化代码
 
     def initUI(self):
         # 拍摄照片
         button = self.findChild(QPushButton, 'paishe')
-        button.clicked.connect(self.onButtonClick)
-        # self.setPixmap('C:\\Users\\26094\Pictures\\powershell_logo_icon_144968.png')
+        button.clicked.connect(self.onTakePicture)
 
+        button = self.findChild(QPushButton, 'body_shibie')
+        button.clicked.connect(self.onBodyShibie)
+
+    def onBodyShibie(self):
+        ret, uuidName = self.savePicture()
+        if ret:
+            Resultimage = body_from_image.body_from_image_By_Url(f'./examples/picture/{uuidName}.jpg')
+            height, width, channel = Resultimage.shape
+            bytes_per_line = 3 * width
+            q_image = QImage(Resultimage.data, width, height, bytes_per_line, QImage.Format_BGR888)
+            picture = self.findChild(QLabel, 'BodyResult')
+            pixmap = QPixmap.fromImage(q_image)
+            scaled_pixmap = pixmap.scaled(picture.size(), aspectRatioMode=Qt.KeepAspectRatio)
+            picture.setPixmap(scaled_pixmap)
+            picture.adjustSize()
+
+    def savePicture(self):
+        if self.image is None:
+            print("还未拍摄照片")
+            return False, None
+        # 生成 UUID
+        uuidName = str(uuid.uuid4())
+        cv2.imwrite(f'./examples/picture/{uuidName}.jpg', self.image)
+        return True, uuidName
+
+    # 识别背景图片
     def setPixmap(self, q_image):
         # 图片显示
         picture = self.findChild(QLabel, 'picture')
@@ -39,7 +67,8 @@ class MyMainWindow(QDialog):
         picture.setPixmap(scaled_pixmap)
         picture.adjustSize()
 
-    def onButtonClick(self):
+    # 拍摄照片
+    def onTakePicture(self):
         self.video = cv2.VideoCapture(0)
         ret, self.image = self.video.read()
         if ret:
@@ -50,6 +79,8 @@ class MyMainWindow(QDialog):
             self.setPixmap(q_image)
         else:
             print("拍摄异常")
+
+
 
 if __name__ == '__main__':
     app = QApplication([])
